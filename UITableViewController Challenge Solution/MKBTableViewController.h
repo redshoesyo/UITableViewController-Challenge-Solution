f409b20d468fd13398e69e44f662d571e92481b5
Mike Boudreau <mikeboudreau@Mikes-MacBook-Air.local>
2014-07-29 22:38:43 -0400

    solved the challenge

---
+++ UITableViewController Challenge Solution/MKBTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKBTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Mike Boudreau on 7/29/14.
+//  Copyright (c) 2014 Mike Boudreau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKBTableViewController : UITableViewController
+
+@end
